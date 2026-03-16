bc1773728e438aae8bac84406dd1b87573a7498c
hannahboxleitner <hbox2809@gmail.com>
2026-03-16 10:17:33 +0100

    added file for main project

---
+++ LLM_replication.R
@@ -0,0 +1,12 @@
+# LLM Replication Study: Replication of Morin & Marttinen Larsson (2025) using Mistral Small 3.2 24B Instruct 2506
+
+# The methodological approach presented by Morin & Marttinen Larsson (2025) is recreated with the code in this script.
+# This script contains the conversation, which is divided into pretraining, training and testing of the model, and also contains analysis and evaluation.
+
+
+# Save transcript as a json log?
+# mention dataset names in the prompt for clarity (in the transcript later)
+
+# Load setup script
+
+# Load data
